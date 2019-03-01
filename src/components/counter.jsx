9ce77e5310d2692cee30256c2a3032d68d211147
unknown <Administrator@V-NAVVE.fareast.corp.microsoft.com>
2019-03-01 19:55:30 +0530

    first commit

---
+++ src/components/counter.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {};
+  render() {
+    return (
+      <div class="container">
+        <h1>Hello World</h1>
+        <button class="btn btn-success">Increment</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
